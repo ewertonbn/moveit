4f8f0b5b18ce0ebc287a1707543b35dae4661bc6
Ewerton <ewertonbn.dev@gmail.com>
2021-02-27 01:28:18 -0300

    completed development application

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/ewertonbn.png" alt="Ewerton Bernardo" />
+      <div>
+        <strong>Ewerton Bernardo</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
